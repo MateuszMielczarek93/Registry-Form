caddb1786ba678d575218b80b6ff0ce84a29fc12
MateuszMielczarek93 <mateuszmielczarek93@gmail.com>
2021-01-10 00:27:04 +0100

    Validacja do poprawy, ustawienie patternu hasla i dodanie wysylania

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators, FormGroup, AbstractControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,29 @@ import { FormControl, Validators} from '@angular/forms';
 })
 export class AppComponent {
   title = 'registry';
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl('', [Validators.required, Validators.email],);
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a valid email';
     }
-
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
-  
 
+  passFormControl = new FormControl('', [Validators.required, Validators.minLength(8),Validators.nullValidator] );
+  confirmFormControl = new FormControl('', [Validators.required, Validators.minLength(8),Validators.nullValidator]);
 
-  
+  checkPasswordControls() {
+    if (this.passFormControl.value !== this.confirmFormControl.value) {
+      this.passFormControl.setErrors({ notEqual: true });
+      this.confirmFormControl.setErrors({ notEqual: true });  
+    } 
 
-}
+    else {
+      this.passFormControl.setErrors(this.passFormControl.errors ? { ...this.passFormControl.errors } : null);
+      this.confirmFormControl.setErrors(this.confirmFormControl.errors ? { ...this.confirmFormControl.errors } : null);
+      
+      
+    }
+  }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
