318a28839282142fcb47b7779176be7d6589908b
MateuszMielczarek93 <mateuszmielczarek93@gmail.com>
2021-01-09 19:25:41 +0100

    Sprawna walidacja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import {FormControl, Validators} from '@angular/forms';
   
 })
 export class AppComponent {
+  title = 'registry';
   email = new FormControl('', [Validators.required, Validators.email]);
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
-      return 'You must enter a value';
+      return 'You must enter a valid email';
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatFormFieldModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
